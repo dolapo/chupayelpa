0ffdec0c75726f6f4082d189afd39d6395a1c2b6
Dolapo Falola <dolapo@foursquare.com>
2011-04-05 11:24:48 -0400

    add foursquare login
    - add a foursquaremixin oauth2mixin
    - add an error.html
    - fix missing __init__.py in minivishnu/frontend

---
+++ minivishnu/frontend/auth.py
@@ -0,0 +1,69 @@
+from tornado.auth import OAuth2Mixin
+from tornado.escape import json_decode
+from tornado.httpclient import AsyncHTTPClient
+
+import json
+import logging
+import urllib
+
+from tornado.options import options, define
+
+class FoursquareMixin(OAuth2Mixin):
+  _OAUTH_AUTHORIZE_URL = 'https://foursquare.com/oauth2/authenticate?'
+  _OAUTH_ACCESS_TOKEN_URL = 'https://foursquare.com/oauth2/access_token?'
+  def get_authenticated_user(self, redirect_uri, client_id, client_secret,
+                             code, callback):
+    url = self._oauth_request_token_url(redirect_uri = redirect_uri,
+                                        code = code,
+                                        client_id = client_id,
+                                        client_secret = client_secret,
+                                        extra_params = {'grant_type': 'authorization_code'})
+    logging.debug('making oauth access token request to %s' % url)
+    http = AsyncHTTPClient()
+    http.fetch(url, self.async_callback(self._on_access_token, callback))
+
+  def _on_access_token(self, callback, response):
+    logging.debug('response from foursquare: %s' % response)
+    if response.error:
+      logging.warning('foursquare auth error: %s' % str(response))
+      callback(None)
+    else:
+      access_token = json.loads(response.body)['access_token']
+      logging.debug('haz access token %s, getting user info', access_token)
+      self.foursquare_request(
+        '/users/self',
+        self.async_callback(self._on_get_user_info, access_token, callback),
+        access_token)
+
+  def _on_get_user_info(self, access_token, callback, response):
+    if response is None:
+      logging.warning('awww, couldn\'t get user info')
+      callback(None)
+    else:
+      logging.debug('response retreived: %s' % response)
+      user = response['response']['user']
+      user['access_token'] = access_token
+      callback(user)
+
+  def foursquare_request(self, path, callback, access_token, post_args = None, **args):
+    '''use post_args to make this a post, otherwise use args'''
+    all_args = {'oauth_token': access_token}
+
+    all_args.update(args)
+    url = 'https://api.foursquare.com/v2%s?%s' % (path, urllib.urlencode(all_args))
+
+    callback = self.async_callback(self._on_foursquare_request, callback)
+    http = AsyncHTTPClient()
+    if post_args is not None:
+      http.fetch(url, method = 'POST', body = urllib.urlencode(post_args),
+                 callback = callback)
+    else:
+      http.fetch(url, callback = callback)
+
+  def _on_foursquare_request(self, callback, response):
+    if response.error:
+      logging.warning('Error response %s from foursquare: %s',
+                      response.error, response.request.url)
+      callback(None)
+    else:
+      callback(json_decode(response.body))
--- minivishnu/frontend/main.py
+++ minivishnu/frontend/main.py
@@ -1,7 +1,10 @@
+from minivishnu.frontend.auth import FoursquareMixin
 from tornado import template
+from tornado.escape import json_encode, json_decode
 from tornado.options import options, define, parse_command_line
 from tornado.web import Application, RequestHandler, url
 
+import json
 import logging
 import os.path
 import tornado.httpserver
@@ -11,13 +14,52 @@ import tornado.web
 define('port', type = int, default = 9009)
 define('debug', type = bool, default = False)
 
-class BaseHandler(RequestHandler):
-  pass
 
-class RootHandler(BaseHandler):
+# Required
+define('base_url', type = str, default = None)
+define('foursquare_client_id', type = str, default = None)
+define('foursquare_client_secret', type = str, default = None)
+
+
+class BaseHandler(RequestHandler):
+  def get_current_user(self):
+    user_cookie = self.get_secure_cookie('user')
+    if user_cookie:
+      return json_decode(user_cookie)
+    else:
+      None
+
+class RootHandler(BaseHandler, FoursquareMixin):
   @tornado.web.asynchronous
   def get(self):
-    self.render('index.html')
+    self.render('index.html',
+                user = self.get_current_user())
+
+class OAuthLoginHandler(BaseHandler, FoursquareMixin):
+  URI = '/auth/foursquare'
+
+  @tornado.web.asynchronous
+  def get(self):
+    if self.get_argument('code', False):
+      self.get_authenticated_user(
+        redirect_uri = options.base_url + OAuthLoginHandler.URI,
+        client_id = options.foursquare_client_id,
+        client_secret = options.foursquare_client_secret,
+        code = self.get_argument('code'),
+        callback = self.async_callback(self._on_auth))
+    else:
+      self.authorize_redirect(redirect_uri = options.base_url + OAuthLoginHandler.URI,
+                              client_id = options.foursquare_client_id,
+                              extra_params = {'response_type': 'code'})
+
+  def _on_auth(self, user):
+    logging.debug('oauth callback, user: %s' % user)
+    if not user:
+      self.render('error.html',
+                  error = 'oauth_token provided, but unable to authenticate')
+    else:
+      self.set_secure_cookie('user', json_encode(user))
+      self.redirect('/')
 
 def main():
   parse_command_line()
@@ -26,7 +68,8 @@ def main():
   template_loader_factory = lambda: template.Loader(template_path)
 
   handlers = [
-    url(r'/$', RootHandler)
+    url(r'/$', RootHandler),
+    url(r'%s$' % OAuthLoginHandler.URI, OAuthLoginHandler)
   ]
 
   app = Application(
